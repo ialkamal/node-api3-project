c5eb399cb4c1ec26d7673fdb72549368a6b26681
Ismail Al Kamal <ismail@nawatt.com>
2021-01-16 19:48:05 +0300

    project setup

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and start it
+const server = require("./api/server");
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}...`);
+});
