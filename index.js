2ca4af2f877e16609b08228d0773392680b24c36
Ismail Al Kamal <ismail@nawatt.com>
2021-01-21 03:03:23 +0300

    added environment variables

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // require your server and start it
 const server = require("./api/server");
+require("dotenv").config();
 
-const PORT = 5000;
+const port = process.env.PORT;
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on port: ${PORT}...`);
+server.listen(port, () => {
+  console.log(`Server is listening on port: ${port}...`);
 });
